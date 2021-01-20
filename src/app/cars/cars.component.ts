abdb46d6661e68dd0c0827ad539fd02a86855454
HabaOleh <gabikoleh25@gmail.com>
2021-01-20 19:01:52 +0200

    Classwork1

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {cars} from 'src/database/data';
+import {ICar} from '../../interfaces';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+  cars: ICar[] = cars;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/interfaces/car.interface.ts
@@ -0,0 +1,10 @@
+export interface ICar {
+  producer: string;
+  model: string;
+  year: number;
+  color: string;
+  type: string;
+  engine: string;
+  volume: number;
+  power: number;
+}
